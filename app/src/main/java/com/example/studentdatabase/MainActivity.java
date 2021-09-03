8fe0bc217d0b045fb60ef3e71b8d60f7f339d775
Akshay <rajputakshay2710@gmail.com>
2021-09-03 14:09:10 +0530

    student-Data-Base

--- app/src/main/java/com/example/studentdatabase/MainActivity.java
+++ app/src/main/java/com/example/studentdatabase/MainActivity.java
@@ -8,30 +8,15 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.ValueRange;
-
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     String Id,Psw;          //Variable to store data.
 
@@ -57,16 +42,10 @@ public class MainActivity extends AppCompatActivity {
                 Id = UserId.getText().toString();
                 Psw = Password.getText().toString();
 
-                LoginDetails Credential = null;
-                try {
-                    Credential = Encryption(Id,Psw);
-                } catch (IOException | GeneralSecurityException e) {
-                    e.printStackTrace();
-                }
-
+                LoginDetails Credential = Encryption(Id,Psw);
 
                 try {
-                    LoginCredentials(Credential);
+                    CredentialCheck(Credential);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -74,48 +53,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-    protected static Credential authorize() throws IOException, GeneralSecurityException{
-
-        InputStream input = MainActivity.class.getResourceAsStream("/credentials.json");
-        if (input == null) throw new FileNotFoundException("Resource not found: " + "/credentials.json");
-
-        GoogleClientSecrets ClientSecrets = GoogleClientSecrets.load(
-                JacksonFactory.getDefaultInstance(), new InputStreamReader(input)
-        );
-
-        List<String> Scopes= Collections.singletonList(SheetsScopes.SPREADSHEETS);
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),JacksonFactory.getDefaultInstance(),ClientSecrets,Scopes)
-                .setDataStoreFactory(new FileDataStoreFactory(new File("tokens")))
-                .setAccessType("offline")
-                .build();
-
-        return new AuthorizationCodeInstalledApp(
-                flow,new LocalServerReceiver()).authorize("user");
-    }
-
-    public static Sheets getSheetsServices() throws IOException, GeneralSecurityException{
-        Credential credential= authorize();
-
-        return new Sheets.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                credential).build();
-    }
-
-    private void LoginCredentials(LoginDetails Credentials) throws IOException {
+    private void CredentialCheck(LoginDetails Credentials) throws IOException {
         LoginDetails loginInput=new LoginDetails();
 
         InputStream row = getResources().openRawResource(R.raw.trial);
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(row, StandardCharsets.UTF_8));
 
-
-
         int i=0;
-        String line= null;
+        String line = new String();
         String Str1="";
         while (true) {
             try {
@@ -123,13 +69,10 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            String[] tokens = new String[2];
-            if (line != null) {
-                tokens = line.split(",");
-            }
-            loginInput.setId(String.valueOf(tokens[0]));
-            loginInput.setPassword(String.valueOf(tokens[1]));
-
+            android.util.Log.d("My Activity", "Line " + line);
+            String[] tokens = line.split(",");
+            loginInput.setId(tokens[0]);
+            loginInput.setPassword(tokens[1]);
 
 
             if(Credentials.getId().equals(loginInput.getId()) && Credentials.getPassword().equals(loginInput.getPassword()))
@@ -147,22 +90,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private LoginDetails Encryption(String Id, String Password) throws IOException, GeneralSecurityException {
-
-        int Key=0;
-        Sheets sheetservices = getSheetsServices();
-        ValueRange Response = sheetservices.spreadsheets().values()
-                .get("1kxejlHtGZJsnB2WCbNah-HeNY1u3Ln18ImdyE0_uQ-4","B2").execute();
-
-        List<List<Object>> values = Response.getValues();
-
-        for(List row: values) {
-            Key= Integer.parseInt((String) row.get(0));
-        }
-
+    private LoginDetails Encryption(String Id,String Password)
+    {
         LoginDetails EncryptedDetails = new LoginDetails();
         char[] id=new char[Id.length()];
         char[] psw=new char[Password.length()];
+        int Key=25;
         for(int i=0;i<Id.length();i++)
             id[i]=(char)((int)Id.charAt(i)+Key);
 
@@ -174,4 +107,3 @@ public class MainActivity extends AppCompatActivity {
         return EncryptedDetails;
     }
 }
-
