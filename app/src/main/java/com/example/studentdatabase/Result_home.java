ecacf3ae408a4fb472e83107a7b874943a6051a4
Akshay <rajputakshay2710@gmail.com>
2021-10-28 23:58:50 +0530

    STUDENT DATABASE

--- app/src/main/java/com/example/studentdatabase/Result_home.java
+++ app/src/main/java/com/example/studentdatabase/Result_home.java
@@ -2,13 +2,31 @@ package com.example.studentdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Result_home extends AppCompatActivity {
+
+    public static final String Sem_Sender = "com.example.studentdatabase.sem";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_home);
+    }
+
+    public void sem1_result(View view){
+        String sem = "1";
+        Intent sem1_intent = new Intent(this,Result.class);
+        sem1_intent.putExtra(Sem_Sender,sem);
+        startActivity(sem1_intent);
+    }
+
+    public void sem2_result(View view){
+        String sem = "2";
+        Intent sem1_intent = new Intent(this,Result.class);
+        sem1_intent.putExtra(Sem_Sender,sem);
+        startActivity(sem1_intent);
     }
 }
