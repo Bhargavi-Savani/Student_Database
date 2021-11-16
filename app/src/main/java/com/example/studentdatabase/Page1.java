b060567166bc0a93b49ef710317a2ef72d3d3866
Akshay <rajputakshay2710@gmail.com>
2021-11-16 19:40:59 +0530

    STUDENT DATABASE

--- app/src/main/java/com/example/studentdatabase/Page1.java
+++ app/src/main/java/com/example/studentdatabase/Page1.java
@@ -1,8 +1,10 @@
 package com.example.studentdatabase;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,16 @@ public class Page1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+
+        Button contact_us_button = findViewById(R.id.Contact_us);
+        contact_us_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://drive.google.com/file/d/1qmj5ZforfgGRmb8Euc9mrwkUqnD2kecx/view?usp=sharing");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
     }
 
     // function to declare type of login as student
